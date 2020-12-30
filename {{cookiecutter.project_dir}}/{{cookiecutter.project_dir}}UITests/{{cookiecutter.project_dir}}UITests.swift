e6eecce7ddc5cfde9abf191486bf77f604447859
mgacy <matt@mgacy.com>
2020-12-30 12:10:14 -0700

    Update test methods

--- {{cookiecutter.project_dir}}/{{cookiecutter.project_dir}}UITests/{{cookiecutter.project_dir}}UITests.swift
+++ {{cookiecutter.project_dir}}/{{cookiecutter.project_dir}}UITests/{{cookiecutter.project_dir}}UITests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class {{ cookiecutter.project_name | replace(' ', '') }}UITests: XCTestCase {
 
-    override func setUp() {
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -19,11 +19,11 @@ class {{ cookiecutter.project_name | replace(' ', '') }}UITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
+    override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
@@ -32,10 +32,10 @@ class {{ cookiecutter.project_name | replace(' ', '') }}UITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() {
+    func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
         }
